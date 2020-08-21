1c2369c48c3f2013ebd889a62b6ae76d941047af
sobhandip1 <sobhandip.samanta@zensar.com>
2020-08-21 19:34:51 +0530

    Adding MVC With Authentication

---
+++ MovieCustomerMvcAppWithAuthentication/Controllers/CustomersController.cs
@@ -0,0 +1,54 @@
+﻿using MovieCustomerMvcAppWithAuthentication.Models;
+using MovieCustomerMvcAppWithAuthentication.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieCustomerMvcAppWithAuthentication.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
+
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var singleCustomer = _context.Customers.Include(c=>c.MembershipType). SingleOrDefault(c => c.Id == id);
+            if (singleCustomer == null)
+                return HttpNotFound();
+            return View(singleCustomer);
+        }
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)//Model Binding
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customers");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ MovieCustomerMvcAppWithAuthentication/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using MovieCustomerMvcAppWithAuthentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieCustomerMvcAppWithAuthentication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context1;
+        public MoviesController()
+        {
+            _context1 = new ApplicationDbContext();
+        }
+        // GET: Movies
+        public ActionResult MovInd()
+        {
+            var movies = _context1.Movies.ToList();
+            return View(movies);
+        }
+    }
+}
